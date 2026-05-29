f0abba50161f29e1c4ee47570bb38d4e950222f3
gemmawoods118-jpg <gemma.woods118@gmail.com>
2026-05-29 17:16:24 +0200

    Add sw.js for push notification service worker

---
+++ sw.js
@@ -0,0 +1,13 @@
+self.addEventListener('push', e => {
+  const data = e.data ? e.data.json() : {};
+  e.waitUntil(self.registration.showNotification(data.title || 'Splittr', {
+    body: data.body || '',
+    icon: '/icon-192.png',
+    badge: '/icon-192.png'
+  }));
+});
+
+self.addEventListener('notificationclick', e => {
+  e.notification.close();
+  e.waitUntil(clients.openWindow('./'));
+});
